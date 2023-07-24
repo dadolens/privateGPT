705c29d4fa97c20fac919874e1cb29edeadd7ce7
Davide Quadrelli <ildado94@gmail.com>
2023-07-24 23:49:59 +0200

    Added web build

--- app.py
+++ app.py
@@ -20,9 +20,9 @@ embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
 db: VectorStore = Chroma(persist_directory=persist_directory, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
 
 
-@app.route('/')
-def ui():
-  return send_from_directory('ui')
+@app.route('/<path:path>')
+def ui(path):
+  return send_from_directory('web', path)
 
 @app.route('/api/ingest', methods=['POST'])
 def ingestApi():
@@ -37,3 +37,6 @@ def queryApi():
 @app.route('/api/stream/query', methods=['POST'])
 def streamQueryApi():
     return Response(query(db, request.json["q"], True), mimetype="application/octet-stream")
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8001)
